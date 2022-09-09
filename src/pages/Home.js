9e3a45ce3f2425dad34dd9f393fad3e742f5016a
Ajulll22 <ajulrizki@gmail.com>
2022-09-09 21:57:44 +0700

    halaman beranda

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CarouselHome from "../components/CarouselHome";
+
+
+const Home = () => {
+    return (
+        <div>
+            <CarouselHome />
+        </div>
+    );
+};
+
+export default Home;
