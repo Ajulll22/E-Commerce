12c8b4310dec74b2893ade97445cbbab6dc9b163
Ajulll22 <ajulrizki@gmail.com>
2022-09-09 21:57:35 +0700

    komponene navbar

---
+++ src/components/Header.js
@@ -0,0 +1,60 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/esm/Col';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+
+function Header() {
+  return (
+    <>
+      <Navbar variant='light' expand="lg">
+        <Container fluid>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: <a href="#login">Mark Otto</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Navbar variant='light' expand="lg">
+        <Container fluid>
+          <Col>
+          </Col>
+          <Col md="8">
+            <Form className="d-flex">
+              <InputGroup>
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="light" id="button-addon2">
+                  Search
+                </Button>
+              </InputGroup>
+            </Form>
+          </Col>
+          <Col>
+            <div className="text-center">
+              <AiOutlineShoppingCart className="cart" onM size={60} />
+            </div></Col>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
