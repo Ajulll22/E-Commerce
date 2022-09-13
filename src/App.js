f2ebb79eb846fd07527c1d939eef93693c74b5fd
Ajulll22 <ajulrizki@gmail.com>
2022-09-13 10:30:17 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
 import Shop from "./pages/Shop";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home";
 import Search from "./pages/Search";
+import Login from "./pages/Login";
+import AuthProvider from "./components/AuthProvider";
 
 function App() {
+
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
+      <AuthProvider>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
+
   );
 }
 
