d71852f1ec1ca5cc516493dfe74396b4dea2aa96
Ajulll22 <ajulrizki@gmail.com>
2022-09-21 21:31:15 +0700

    .

--- src/components/ListProduct.js
+++ src/components/ListProduct.js
@@ -1,21 +1,43 @@
+import axios from 'axios';
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/esm/Col'
 import { FiPlusCircle } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+import swal from 'sweetalert';
+import { API_URL } from '../utils/Api';
 import { numberWithCommas } from '../utils/CostFormat';
 
 
 const ListProduct = ({ product }) => {
 
+    const addCart = async (id_product) => {
+        try {
+            await axios.post(API_URL + "cart", { id_product }, {
+                withCredentials: true
+            })
+            swal({
+                title: "Success",
+                text: "Berhasil Masuk Keranjang ",
+                icon: "success",
+                button: false,
+                timer: 1500,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
     return (
-        <Col md={4} xs={6} className="mb-3">
+        <Col sm={6} md={6} lg={4} className="mb-3 product-col">
             <Card className='card-product'>
                 <Card.Img variant="top" src={product.url_product} />
                 <Card.Body>
-                    <Card.Title>{product.nama_product}</Card.Title>
+                    <Card.Title><Link className='link-nav add-to-cart' to={'/shop/' + product.id_product}>{product.nama_product}</Link></Card.Title>
 
                     <div className="d-flex justify-content-between align-items-center">
-                        <FiPlusCircle className='add-to-cart' size={40} />
+                        <FiPlusCircle onClick={() => addCart(product.id_product)} className='add-to-cart' size={40} />
 
                         <h5 style={{ color: "#53ADD1" }} >Rp. {numberWithCommas(product.harga_product)}</h5>
                     </div>
