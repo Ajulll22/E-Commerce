931ee9f2731764dc59686ba261396809c91b7903
Ajulll22 <ajulrizki@gmail.com>
2022-09-21 21:31:04 +0700

    .

--- src/components/DetailCart.js
+++ src/components/DetailCart.js
@@ -1,8 +1,10 @@
 import { MDBCol, MDBTypography } from 'mdb-react-ui-kit'
 import React from 'react'
+import { Image } from 'react-bootstrap'
 import ListCart from './ListCart'
+import no_data from '../images/not-found.svg'
 
-const DetailCart = () => {
+const DetailCart = ({ carts, getCart }) => {
     return (
         <MDBCol lg="8">
             <div className="p-5">
@@ -11,13 +13,21 @@ const DetailCart = () => {
                         Shopping Cart
                     </MDBTypography>
                     <MDBTypography className="mb-0 text-muted">
-                        3 items
+                        {carts.length} items
                     </MDBTypography>
                 </div>
 
                 <hr className="my-4" />
+                {carts.length ?
+                    carts.map((cart) => (
+                        <ListCart key={cart.id_cart} getCart={getCart} cart={cart} />
+                    ))
+                    : <div className='text-center mt-3'>
+                        <Image width={'50%'} height={'50%'} src={no_data} />
+                        <h2>Data Not Found</h2>
+                    </div>
+                }
 
-                <ListCart />
 
             </div>
         </MDBCol>
