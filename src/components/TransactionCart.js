a26917d13bb1dd85120d2bcc5d673b64775ecaf7
Ajulll22 <ajulrizki@gmail.com>
2022-09-24 16:01:41 +0700

    .

--- src/components/TransactionCart.js
+++ src/components/TransactionCart.js
@@ -1,13 +1,29 @@
 import { MDBCol, MDBInput, MDBTypography } from 'mdb-react-ui-kit'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { numberWithCommas } from '../utils/CostFormat'
 import Address from './Address'
+import ErrorMessage from './ErrorMessage'
 
 const TransactionCart = ({ carts }) => {
     const total_trx = carts.reduce(function (result, item) {
         return result + item.total_harga;
     }, 0);
+    const [address, setAddress] = useState('')
+    const [show, setShow] = useState(false)
+    const [error, setError] = useState([])
+
+    const CheckOut = () => {
+        if (carts.length === 0) {
+            setError(['Keranjang Kosong'])
+            setShow(true)
+            console.log(1);
+        } else if (address.length < 5) {
+            setError(['Alamat Kosong'])
+            setShow(true)
+            console.log(1);
+        }
+    }
 
     return (
         <MDBCol lg="4" className="bg-grey">
@@ -17,8 +33,12 @@ const TransactionCart = ({ carts }) => {
                 </MDBTypography>
 
                 <hr className="my-4" />
-
-                <Address />
+                {
+                    error.length ?
+                        <ErrorMessage error={error} show={show} setShow={setShow} />
+                        : null
+                }
+                <Address address={address} setAddress={setAddress} />
 
                 <MDBTypography tag="h5" className="text-uppercase mb-3">
                     Give code
@@ -37,7 +57,7 @@ const TransactionCart = ({ carts }) => {
                     <MDBTypography tag="h5">Rp. {numberWithCommas(total_trx)}</MDBTypography>
                 </div>
 
-                <Button variant='secondary'>Check Out</Button>
+                <Button onClick={CheckOut} variant='secondary'>Check Out</Button>
             </div>
         </MDBCol>
     )
