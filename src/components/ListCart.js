d1d900d8de391b7ad395fd5bbac2078128c0c2a2
Ajulll22 <ajulrizki@gmail.com>
2022-09-30 20:18:01 +0700

    .

--- src/components/ListCart.js
+++ src/components/ListCart.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 import { MDBCardImage, MDBCol, MDBRow, MDBTypography } from 'mdb-react-ui-kit'
 import React, { useState } from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, Form } from 'react-bootstrap'
 import { HiMinus, HiPlus, HiTrash } from 'react-icons/hi'
 import { API_URL } from '../utils/Api'
 import { numberWithCommas } from '../utils/CostFormat'
 import swal from 'sweetalert'
 
-const ListCart = ({ cart, getCart }) => {
+const ListCart = ({ getTotalHarga, idCheck, setIdCheck, cart, getCart }) => {
     const [jumlah, setJumlah] = useState(cart.jumlah)
     const [totalHarga, setTotalHarga] = useState(cart.total_harga)
 
@@ -17,6 +17,7 @@ const ListCart = ({ cart, getCart }) => {
         })
         setTotalHarga(response.data.total_harga)
         await getCart()
+        getTotalHarga()
     }
 
     const tambah = (id_cart) => {
@@ -54,28 +55,47 @@ const ListCart = ({ cart, getCart }) => {
 
     }
 
+    const checkBuy = (e) => {
+        const { id, checked } = e.target;
+        console.log(id);
+        if (checked) {
+            setIdCheck([...idCheck, parseInt(id)])
+        } else {
+            setIdCheck(idCheck.filter((e) => e !== parseInt(id)))
+        }
+        console.log(idCheck);
+    }
+
     return (
         <>
+            <MDBTypography tag="h6" className="text-black mb-2">
+                {cart.product.nama_product}
+            </MDBTypography>
             <MDBRow className="mb-4 d-flex justify-content-between align-items-center">
+                <MDBCol md="1" lg="1" xl="1">
+                    <Form.Check
+                        type="checkbox"
+                        name="buy"
+                        id={cart.id_cart}
+                        onChange={(e) => checkBuy(e)}
+                    />
+                </MDBCol>
                 <MDBCol md="2" lg="2" xl="2">
                     <MDBCardImage
                         src={cart.product.url_product}
                         fluid className="rounded-3" alt="" />
                 </MDBCol>
-                <MDBCol md="3" lg="3" xl="3">
+                <MDBCol md="2" lg="2" xl="2">
                     <MDBTypography tag="h6" className="text-muted">
                         {cart.product.category.nama_category}
                     </MDBTypography>
-                    <MDBTypography tag="h6" className="text-black mb-0">
-                        {cart.product.nama_product}
-                    </MDBTypography>
                 </MDBCol>
                 <MDBCol md="3" lg="3" xl="3" className="d-flex align-items-center">
-                    <Button onClick={() => kurang(cart.id_cart)} variant="secondary" size="sm" className="m-2"><HiMinus /></Button>
+                    <Button onClick={() => kurang(cart.id_cart)} variant="secondary" size="sm" className="me-2"><HiMinus /></Button>
 
                     <strong>{jumlah}</strong>
 
-                    <Button onClick={() => tambah(cart.id_cart)} variant="secondary" size="sm" className="m-2"><HiPlus /></Button>
+                    <Button onClick={() => tambah(cart.id_cart)} variant="secondary" size="sm" className="ms-2"><HiPlus /></Button>
                 </MDBCol>
                 <MDBCol md="3" lg="3" xl="3" className="text-end">
                     <MDBTypography tag="h6" className="mb-0">
