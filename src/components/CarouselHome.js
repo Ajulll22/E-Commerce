8373301a0ccfc7a5e82d6d5631ce9f605d124661
Ajulll22 <ajulrizki@gmail.com>
2022-09-30 20:17:50 +0700

    .

--- src/components/CarouselHome.js
+++ src/components/CarouselHome.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Carousel from "react-bootstrap/Carousel";
-import image from "../images/bg.jpg";
+import image1 from "../images/carousel-1.png";
+import image2 from '../images/carousel-2.png'
 import { Link } from "react-router-dom";
 import { FiArrowUpRight } from "react-icons/fi";
 
@@ -8,7 +9,23 @@ const CarouselHome = () => {
     return (
         <Carousel>
             <Carousel.Item>
-                <img className="d-block w-100" src={image} alt="First slide" />
+                <img className="d-block w-100" src={image1} alt="First slide" />
+                <Carousel.Caption>
+
+
+                    <h4 className="link-home .text-dark">
+                        <Link
+                            className="mt-4"
+                            style={{ textDecoration: "none", color: "black" }}
+                            to={"/shop"}
+                        >
+                            Belanja Sekarang <FiArrowUpRight className="rotate" />
+                        </Link>
+                    </h4>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="d-block w-100" src={image2} alt="First slide" />
                 <Carousel.Caption>
 
 
