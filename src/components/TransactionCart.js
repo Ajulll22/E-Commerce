44407c845a897f1a93691b067cf105ff66a66218
Ajulll22 <ajulrizki@gmail.com>
2022-09-30 20:18:11 +0700

    .

--- src/components/TransactionCart.js
+++ src/components/TransactionCart.js
@@ -1,29 +1,17 @@
 import { MDBCol, MDBInput, MDBTypography } from 'mdb-react-ui-kit'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { numberWithCommas } from '../utils/CostFormat'
 import Address from './Address'
+import AuthContext from './AuthContext'
 import ErrorMessage from './ErrorMessage'
 
-const TransactionCart = ({ carts }) => {
+const TransactionCart = ({ error, show, setShow, onCheckout, carts }) => {
     const total_trx = carts.reduce(function (result, item) {
         return result + item.total_harga;
     }, 0);
-    const [address, setAddress] = useState('')
-    const [show, setShow] = useState(false)
-    const [error, setError] = useState([])
-
-    const CheckOut = () => {
-        if (carts.length === 0) {
-            setError(['Keranjang Kosong'])
-            setShow(true)
-            console.log(1);
-        } else if (address.length < 5) {
-            setError(['Alamat Kosong'])
-            setShow(true)
-            console.log(1);
-        }
-    }
+    const { user, getUserLogin } = useContext(AuthContext)
+    const [address, setAddress] = useState(user.alamat_user)
 
     return (
         <MDBCol lg="4" className="bg-grey">
@@ -38,7 +26,7 @@ const TransactionCart = ({ carts }) => {
                         <ErrorMessage error={error} show={show} setShow={setShow} />
                         : null
                 }
-                <Address address={address} setAddress={setAddress} />
+                <Address user={user} getUserLogin={getUserLogin} address={address} setAddress={setAddress} />
 
                 <MDBTypography tag="h5" className="text-uppercase mb-3">
                     Give code
@@ -51,13 +39,13 @@ const TransactionCart = ({ carts }) => {
                 <hr className="my-4" />
 
                 <div className="d-flex justify-content-between mb-5">
-                    <MDBTypography tag="h5" className="text-uppercase">
-                        Total price
+                    <MDBTypography tag="h6" >
+                        Total Harga
                     </MDBTypography>
-                    <MDBTypography tag="h5">Rp. {numberWithCommas(total_trx)}</MDBTypography>
+                    <MDBTypography tag="h6">Rp. {numberWithCommas(total_trx)}</MDBTypography>
                 </div>
 
-                <Button onClick={CheckOut} variant='secondary'>Check Out</Button>
+                <Button className={carts.length === 0 && "disabled"} onClick={onCheckout} variant='secondary'>Check Out</Button>
             </div>
         </MDBCol>
     )
