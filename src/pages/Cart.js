d502f9af2f4a1ba10f8af69bfcc9737b3ebf880e
Ajulll22 <ajulrizki@gmail.com>
2022-09-30 20:18:17 +0700

    .

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -11,19 +11,61 @@ import DetailCart from '../components/DetailCart';
 import TransactionCart from '../components/TransactionCart';
 import axios from 'axios';
 import { API_URL } from '../utils/Api';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
     const [carts, setCarts] = useState([])
+    const [cartCheck, setCartCheck] = useState([])
+    const [idCheck, setIdCheck] = useState([])
+    const [show, setShow] = useState(false)
+    const [error, setError] = useState([])
+
+    let navigate = useNavigate();
 
     useEffect(() => {
         getCart()
     }, [])
+
+    useEffect(() => {
+        getTotalHarga()
+    }, [idCheck])
 
     const getCart = async () => {
         const response = await axios.get(API_URL + "cart", {
             withCredentials: true
         })
         setCarts(response.data)
+    }
+
+    const getTotalHarga = async () => {
+        const response = await axios.post(API_URL + "cart/checked", { id_check: idCheck },
+            {
+                withCredentials: true
+            })
+        console.log(response.data);
+        setCartCheck(response.data)
+    }
+
+    const onCheckout = async () => {
+        try {
+            const response = await axios.post(API_URL + "transaction", { id_cart: idCheck },
+                {
+                    withCredentials: true
+                })
+            navigate("/success")
+        } catch (error) {
+            const message = error.response.data.message
+            if (typeof message === "object") {
+                setError(error.response.data.message)
+            } else {
+                setError([error.response.data.message])
+            }
+            setShow(true)
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
     }
 
     return (
@@ -37,9 +79,19 @@ const Cart = () => {
                             <MDBCard className="card-registration card-registration-2" style={{ borderRadius: "15px" }}>
                                 <MDBCardBody className="p-0">
                                     <MDBRow className="g-0">
-                                        <DetailCart getCart={getCart} carts={carts} />
+                                        <DetailCart
+                                            getTotalHarga={getTotalHarga}
+                                            idCheck={idCheck}
+                                            setIdCheck={setIdCheck}
+                                            getCart={getCart}
+                                            carts={carts} />
 
-                                        <TransactionCart carts={carts} />
+                                        <TransactionCart
+                                            onCheckout={onCheckout}
+                                            error={error}
+                                            show={show}
+                                            setShow={setShow}
+                                            carts={cartCheck} />
                                     </MDBRow>
                                 </MDBCardBody>
                             </MDBCard>
