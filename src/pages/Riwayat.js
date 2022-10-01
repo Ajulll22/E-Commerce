637c414959438c71b55e3ce7db86ab7304c53ad5
Ajulll22 <ajulrizki@gmail.com>
2022-10-01 21:21:47 +0700

    .

--- src/pages/Riwayat.js
+++ src/pages/Riwayat.js
@@ -1,46 +1,42 @@
-import React from 'react'
-import { Badge, Card, Col, Container, Image, Row } from 'react-bootstrap'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Container, Image, Row } from 'react-bootstrap'
 import Header from '../components/Header'
+import ListTransaction from '../components/ListTransaction'
+import { API_URL } from '../utils/Api'
+import no_data from '../images/not-found.svg'
 
 const Riwayat = () => {
+    const [transactions, setTransactions] = useState([])
+
+    useEffect(() => {
+        getTransactions()
+    }, [])
+
+    const getTransactions = async () => {
+        const response = await axios.get(API_URL + "transaction", {
+            withCredentials: true
+        })
+        setTransactions(response.data)
+    }
+
     return (
         <>
             <Header />
             <hr />
             <Container className='mt-5'>
                 <Row>
-                    <Card className='shadow-sm mb-2'>
-                        <Card.Body>
-                            <div className="d-flex justify-content-between align-items-center">
-                                <p>26 Jul 20022</p>
-                                <p className='rounded bg-warning align-items-center mx-3 px-1 fst-italic'>Dikirim</p>
-                                <p>IVR/20220726/XXII/VII/1272959144</p>
+                    {
+                        transactions.length
+                            ? transactions.map((transaction) => (<ListTransaction key={transaction.id_trx} transaction={transaction} />))
+                            : <div className='text-center mt-3'>
+                                <Image width={'50%'} height={'50%'} src={no_data} />
+                                <h2>Data Not Found</h2>
                             </div>
-                            <Row className='d-flex justify-content-between align-items-center'>
-                                <hr className='hr-no-margin' />
-                                <Col sm={12} md={12} lg={2}>
-                                    <Image fluid className="rounded-3" src='http://localhost:5000/images/product-20220923214403.webp' />
-                                </Col>
-                                <Col sm={12} md={12} lg={10}>
-                                    <p className='fw-bold'>Alloy Origins Core Tenkeyless Mechanical Gaming Keyboard</p>
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <div>
-                                            <p className='text-muted'>Keyboard</p>
-                                            <p>1 barang x 150,000</p>
-                                            <p>+1 Produk Lainnya</p>
-                                        </div>
-                                        <div>Total Harga : Rp. 150,000</div>
-                                    </div>
-                                </Col>
-                                <hr className='hr-no-margin' />
-                            </Row>
-                            <div className='text-end mt-1'>
-                                <strong>Lihat Detail</strong>
-                            </div>
-                        </Card.Body>
-                    </Card>
+                    }
                 </Row>
             </Container>
+            <hr />
         </>
     )
 }
