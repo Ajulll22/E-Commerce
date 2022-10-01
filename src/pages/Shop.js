23aa0813c8900c903eaf304093ce81920221c578
Ajulll22 <ajulrizki@gmail.com>
2022-10-01 21:21:54 +0700

    .

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -13,6 +13,7 @@ import no_data from '../images/not-found.svg'
 import { Button } from 'react-bootstrap'
 import { AiOutlinePlusSquare } from 'react-icons/ai'
 import AuthContext from '../components/AuthContext'
+import Paginate from '../components/Paginate'
 
 
 const Shop = () => {
@@ -21,7 +22,20 @@ const Shop = () => {
     const [categoryQuery, setCategoryQuery] = useSearchParams();
     const [categorySelect, setCategorySelect] = useState(categoryQuery.get('id_category') || '')
     const [namaCategory, setNamaCategory] = useState('')
+    const [page, setPage] = useState(categoryQuery.get('page') || 0);
+    const [pages, setPages] = useState(0);
+
     const { user } = useContext(AuthContext)
+
+    const changePage = ({ selected }) => {
+        setPage(selected)
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+        categoryQuery.set("page", selected)
+        setCategoryQuery(categoryQuery)
+    }
 
     useEffect(() => {
         getProducts()
@@ -30,18 +44,34 @@ const Shop = () => {
 
     useEffect(() => {
         getProducts()
-    }, [categorySelect])
+    }, [categorySelect, page])
 
 
     const onCategorySelect = (id_category, nama_category) => {
-        setCategorySelect(id_category)
-        setNamaCategory(nama_category)
-        setCategoryQuery({ id_category })
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+        if (id_category === categorySelect) {
+            setCategorySelect('')
+            setNamaCategory('')
+            categoryQuery.delete("id_category")
+            setCategoryQuery(categoryQuery)
+        } else {
+            setCategorySelect(id_category)
+            setNamaCategory(nama_category)
+            categoryQuery.delete("page")
+            categoryQuery.set("id_category", id_category)
+            setCategoryQuery(categoryQuery)
+        }
+        setPage(0)
     }
 
     const getProducts = async () => {
-        const response = await axios.get(API_URL + "products?id_category=" + categorySelect)
+        const response = await axios.get(API_URL + "products?id_category=" + categorySelect + "&page=" + page)
         setProducts(response.data.result)
+        setPage(response.data.page);
+        setPages(response.data.totalPage);
     }
 
     const getCategories = async () => {
@@ -53,7 +83,8 @@ const Shop = () => {
     return (
         <div>
             <Header />
-            <div className="px-4 pt-5 my-5 text-center border-top">
+            <hr />
+            <div className="px-4 pt-5 my-5 text-center">
                 <h5 className="title display-4 fw-bold">Gaming {namaCategory || 'Gear'}</h5>
             </div>
 
@@ -67,16 +98,21 @@ const Shop = () => {
                 <Row>
                     <ListCategory categorySelect={categorySelect} onCategorySelect={onCategorySelect} categories={categories} />
                     <Col className='mx-2 my-auto'>
+
                         <Row>
                             {products.length ?
-                                products.map((product) => (
-                                    <ListProduct key={product.id_product} product={product} />
-                                ))
+                                <>
+                                    <Paginate pages={pages} changePage={changePage} />
+                                    {products.map((product) => (
+                                        <ListProduct key={product.id_product} product={product} />
+                                    ))}
+                                </>
                                 : <div className='text-center mt-3'>
                                     <Image width={'50%'} height={'50%'} src={no_data} />
                                     <h2>Data Not Found</h2>
                                 </div>
                             }
+
                         </Row>
                     </Col>
                 </Row>
